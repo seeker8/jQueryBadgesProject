884c8c35b08c6567fed8877d0fbb9998192c6b29
seeker8 <chuyo_gk87@yahoo.com.mx>
2017-02-13 02:31:32 +0000

    Add ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
   // your code will go here
-
+  $.get({
+     url: 'https://www.codeschool.com/users/1947520.json', 
+     dataType: 'jsonp',
+     success: function(data){
+         
+     }
+  });
 });
